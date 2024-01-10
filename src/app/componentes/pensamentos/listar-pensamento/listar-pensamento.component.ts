b7750fa728d216f1d19f603d99eb6d8d29b15564
Gabriel120195 <gabriel-1295@hotmail.com>
2024-01-10 16:20:37 -0300

    Finalizando projeto

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -12,9 +13,12 @@ export class ListarPensamentoComponent implements OnInit {
 
   ];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) =>{
+      this.listaPensamentos = listaPensamentos;
+    })
   }
 
 }
